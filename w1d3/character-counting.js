844c6b747ce75476fc9e344a282bbe70492dea4a
esplett <estherina@gmail.com>
2018-11-28 16:35:59 +0000

    first commit

---
+++ w1d3/character-counting.js
@@ -0,0 +1,24 @@
+
+
+
+-countLetters return unique characters that exist in a
+string that is passed into a function
+-instances each letter was found in string
+-returns object that represents stats for sentence,
+all chars found in string, and their counts
+
+
+
+countLetters("lighthouse in the house")
+{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}
