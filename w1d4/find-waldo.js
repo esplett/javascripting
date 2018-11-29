eea19953ee83dcaa9a30b3e8e5a6ca26a92992f0
esplett <estherina@gmail.com>
2018-11-29 17:04:08 +0000

    used forEach as a loop

--- w1d4/find-waldo.js
+++ w1d4/find-waldo.js
@@ -1,11 +1,10 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+findWaldo.forEach(actionWhenFound(arr, found) {
     if (arr[i] === "Waldo") {
-      found(i);  // execute callback
-    }
+      found();  // execute callback
+    });
   }
-}
+
 
 function actionWhenFound(place) {
   console.log(`Found Waldo at index ${place}!`);
@@ -14,8 +13,8 @@ function actionWhenFound(place) {
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound,);
 
 
+
 // Modify the callback function in the previous example so that logs the
 // index of the array where Waldo is found, ie. "Found Waldo at index 2!".
 // (You will need to modify actionWhenFound to receive the index.)
-
 
